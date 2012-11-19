ce1277ce5e518a66ff678b077ebb5bfbd107d62f
Johannes Utzig <jutzig.dev@googlemail.com>
2012-11-19 22:41:36 +0100

    implemented add + commit and update

--- de.jutzig.jabylon.team.cvs/src/main/java/de/jutzig/jabylon/team/cvs/impl/CVSTeamProvider.java
+++ de.jutzig.jabylon.team.cvs/src/main/java/de/jutzig/jabylon/team/cvs/impl/CVSTeamProvider.java
@@ -5,7 +5,10 @@ package de.jutzig.jabylon.team.cvs.impl;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.netbeans.lib.cvsclient.CVSRoot;
@@ -14,8 +17,10 @@ import org.netbeans.lib.cvsclient.admin.StandardAdminHandler;
 import org.netbeans.lib.cvsclient.command.CommandAbortedException;
 import org.netbeans.lib.cvsclient.command.CommandException;
 import org.netbeans.lib.cvsclient.command.GlobalOptions;
+import org.netbeans.lib.cvsclient.command.add.AddCommand;
 import org.netbeans.lib.cvsclient.command.checkout.CheckoutCommand;
 import org.netbeans.lib.cvsclient.command.commit.CommitCommand;
+import org.netbeans.lib.cvsclient.command.update.UpdateCommand;
 import org.netbeans.lib.cvsclient.commandLine.BasicListener;
 import org.netbeans.lib.cvsclient.connection.AuthenticationException;
 import org.netbeans.lib.cvsclient.connection.Connection;
@@ -35,17 +40,18 @@ import org.slf4j.LoggerFactory;
 
 import de.jutzig.jabylon.common.team.TeamProviderException;
 import de.jutzig.jabylon.common.util.PreferencesUtil;
+import de.jutzig.jabylon.properties.DiffKind;
 import de.jutzig.jabylon.properties.Project;
 import de.jutzig.jabylon.properties.ProjectVersion;
+import de.jutzig.jabylon.properties.PropertiesFactory;
 import de.jutzig.jabylon.properties.PropertyFileDescriptor;
 import de.jutzig.jabylon.properties.PropertyFileDiff;
 
 /**
  * @author Johannes Utzig (jutzig.dev@googlemail.com)
- *
+ * 
  */
 public class CVSTeamProvider implements de.jutzig.jabylon.common.team.TeamProvider {
-
 
 	private static final Logger logger = LoggerFactory.getLogger(CVSTeamProvider.class);
 
@@ -57,7 +63,7 @@ public class CVSTeamProvider implements de.jutzig.jabylon.common.team.TeamProvid
 			client = theClient;
 			final String fullPath = project.absolutPath().toFileString();
 			monitor.beginTask("Checkout", IProgressMonitor.UNKNOWN);
-			// TODO: is  there  a way  to get an estimate at least?
+			// TODO: is there a way to get an estimate at least?
 			final CheckoutCommand checkout = new CheckoutCommand();
 			client.getEventManager().addCVSListener(new ProgressMonitorListener(monitor, client, fullPath));
 
@@ -90,45 +96,95 @@ public class CVSTeamProvider implements de.jutzig.jabylon.common.team.TeamProvid
 
 	@Override
 	public void commit(ProjectVersion project, final IProgressMonitor monitor) {
-        Client client = null;
-        try {
-            final Client theClient = createClient(project);
-            client = theClient;
-            final String fullPath = project.absolutPath().toFileString();
-            monitor.beginTask("Committing Changes", IProgressMonitor.UNKNOWN);
-            // TODO: is  there  a way  to get an estimate at least?
-            final CommitCommand commit = new CommitCommand();
-            client.getEventManager().addCVSListener(new ProgressMonitorListener(monitor, client, fullPath));
+		Client client = null;
+		try {
+			Client theClient = createClient(project);
+			client = theClient;
+			final String fullPath = project.absolutPath().toFileString();
+			monitor.beginTask("Committing Changes", IProgressMonitor.UNKNOWN);
+			
 
-            commit.setRecursive(true);
-            commit.setMessage("Jabylon Auto-Sync Up");
-//            commit.setToRevisionOrBranch(project.getName());
-            commit.setFiles(new File(fullPath).listFiles());
-            client.executeCommand(commit, getGlobalOptions(project.getParent()));
-        } catch (AuthenticationException e) {
-            throw new TeamProviderException("Commit failed", e);
-        } catch (CommandAbortedException e) {
-            throw new TeamProviderException("Commit failed", e);
-        } catch (CommandException e) {
-            throw new TeamProviderException("Commit failed", e);
-        } catch (Exception e) {
-            throw new TeamProviderException("Commit failed", e);
-        } finally {
-            if (client != null)
-                try {
-                    client.getConnection().close();
-                } catch (IOException e) {
-                    logger.error("Failed to close client connection", e);
-                }
-            if (monitor != null)
-                monitor.done();
-        }
+			
+			// TODO: is there a way to get an estimate at least?
+			client.getEventManager().addCVSListener(new ProgressMonitorListener(monitor, client, fullPath));
+			
+			File[] filesToAdd = calculateMissingFiles(client,new File(fullPath));
+			if(filesToAdd.length>0)
+			{
+				AddCommand add = new AddCommand();
+				add.setMessage("Jabylon Auto-Sync Up");
+				add.setFiles(filesToAdd);
+				client.executeCommand(add, getGlobalOptions(project.getParent()));
+				
+				try {
+					client.getConnection().close();
+				} catch (IOException e) {
+					logger.error("Failed to close client connection", e);
+				}
+				
+				//must create a new client once the add succeeded
+				theClient = createClient(project);
+				client = theClient;
+				
+			}
+			
+			final CommitCommand commit = new CommitCommand();
+			commit.setRecursive(true);
+			commit.setMessage("Jabylon Auto-Sync Up");
+			// commit.setToRevisionOrBranch(project.getName());
+			commit.setFiles(new File(fullPath).listFiles());
+			client.executeCommand(commit, getGlobalOptions(project.getParent()));
+		} catch (AuthenticationException e) {
+			throw new TeamProviderException("Commit failed", e);
+		} catch (CommandAbortedException e) {
+			throw new TeamProviderException("Commit failed", e);
+		} catch (CommandException e) {
+			throw new TeamProviderException("Commit failed", e);
+		} catch (Exception e) {
+			throw new TeamProviderException("Commit failed", e);
+		} finally {
+			if (client != null)
+				try {
+					client.getConnection().close();
+				} catch (IOException e) {
+					logger.error("Failed to close client connection", e);
+				}
+			if (monitor != null)
+				monitor.done();
+		}
 
 	}
 
-	@Override
-	public void commit(PropertyFileDescriptor descriptor, IProgressMonitor monitor){
+	private File[] calculateMissingFiles(Client client, File parentDir) throws IOException {
+		List<File> filesToAdd = new ArrayList<File>();
+		addMissingFiles(parentDir,client,filesToAdd);
+		return filesToAdd.toArray(new File[filesToAdd.size()]);
+	}
 
+	private void addMissingFiles(File parentDir, Client client, List<File> filesToAdd) throws IOException {
+		Set<File> knownFiles = client.getAllFiles(parentDir);
+		File[] files = parentDir.listFiles();
+		for (File file : files) {
+			if(file.isDirectory())
+			{
+				if(file.getName().equals("CVS"))
+					continue;
+				addMissingFiles(file, client, filesToAdd);
+			}
+			else
+			{
+				if(!knownFiles.contains(file))
+				{
+					logger.info("CVS ADD {}",file);
+					filesToAdd.add(file);
+				}
+			}
+		}
+		
+	}
+
+	@Override
+	public void commit(PropertyFileDescriptor descriptor, IProgressMonitor monitor) {
 
 	}
 
@@ -147,8 +203,16 @@ public class CVSTeamProvider implements de.jutzig.jabylon.common.team.TeamProvid
 		}
 
 		Client client = new Client(connection, new StandardAdminHandler());
-		File file = new File(projectVersion.absoluteFilePath().toFileString());
-		client.setLocalPath(file.getAbsolutePath());
+		File parentDir = new File(projectVersion.absoluteFilePath().toFileString());
+		if(parentDir.listFiles().length==1)
+		{
+			//the checkout is already done. Use the module as the client local path
+			client.setLocalPath(parentDir.listFiles()[0].getAbsolutePath());
+		}
+		else
+		{
+			client.setLocalPath(parentDir.getAbsolutePath());			
+		}
 		return client;
 	}
 
@@ -160,105 +224,174 @@ public class CVSTeamProvider implements de.jutzig.jabylon.common.team.TeamProvid
 	}
 
 	@Override
-	public Collection<PropertyFileDiff> update(ProjectVersion project,
-			IProgressMonitor monitor) throws TeamProviderException {
-		// TODO Auto-generated method stub
-		return null;
+	public Collection<PropertyFileDiff> update(ProjectVersion project, IProgressMonitor monitor) throws TeamProviderException {
+		Client client = null;
+		try {
+			final Client theClient = createClient(project);
+			client = theClient;
+			final String fullPath = project.absolutPath().toFileString();
+			monitor.beginTask("Updating", IProgressMonitor.UNKNOWN);
+			// TODO: is there a way to get an estimate at least?
+			UpdateCommand command = new UpdateCommand();
+			
+			DiffListener diffListener = new DiffListener(monitor, client, fullPath);
+			client.getEventManager().addCVSListener(diffListener);
+			command.setRecursive(true);
+			command.setPruneDirectories(true);
+			command.setFiles(new File(fullPath).listFiles());
+			command.setBuildDirectories(true);
+			client.executeCommand(command, getGlobalOptions(project.getParent()));
+			return diffListener.getDiff();
+		} catch (AuthenticationException e) {
+			throw new TeamProviderException("Update failed", e);
+		} catch (CommandAbortedException e) {
+			throw new TeamProviderException("Update failed", e);
+		} catch (CommandException e) {
+			throw new TeamProviderException("Update failed", e);
+		} catch (Exception e) {
+			throw new TeamProviderException("Update failed", e);
+		} finally {
+			if (client != null)
+				try {
+					client.getConnection().close();
+				} catch (IOException e) {
+					logger.error("Failed to close client connection", e);
+				}
+			if (monitor != null)
+				monitor.done();
+		}
 	}
 
 	@Override
-	public Collection<PropertyFileDiff> update(
-			PropertyFileDescriptor descriptor, IProgressMonitor monitor)
-			throws TeamProviderException {
+	public Collection<PropertyFileDiff> update(PropertyFileDescriptor descriptor, IProgressMonitor monitor) throws TeamProviderException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 }
 
+class ProgressMonitorListener extends BasicListener {
 
-class ProgressMonitorListener extends BasicListener
+	private static final long serialVersionUID = 1L;
+	private final IProgressMonitor monitor;
+	private final Client client;
+	private String basePath;
+
+	public ProgressMonitorListener(IProgressMonitor monitor, Client client, String basePath) {
+		super();
+		this.monitor = monitor;
+		this.client = client;
+		this.basePath = basePath;
+	}
+
+	@Override
+	public void moduleExpanded(ModuleExpansionEvent arg0) {
+		super.moduleExpanded(arg0);
+		monitor.setTaskName(arg0.getModule());
+		monitor.worked(1);
+	}
+
+	@Override
+	public void fileAdded(FileAddedEvent arg0) {
+		super.fileAdded(arg0);
+		if (monitor.isCanceled())
+			client.abort();
+		String path = truncatePath(arg0.getFilePath());
+		monitor.subTask(path);
+		monitor.worked(1);
+	}
+
+	@Override
+	public void fileInfoGenerated(FileInfoEvent arg0) {
+		super.fileInfoGenerated(arg0);
+		if (monitor.isCanceled())
+			client.abort();
+	}
+
+	@Override
+	public void fileRemoved(FileRemovedEvent arg0) {
+		super.fileRemoved(arg0);
+		if (monitor.isCanceled())
+			client.abort();
+		String path = truncatePath(arg0.getFilePath());
+		monitor.subTask(path);
+		monitor.worked(1);
+	}
+
+	@Override
+	public void fileToRemove(FileToRemoveEvent arg0) {
+		super.fileToRemove(arg0);
+		if (monitor.isCanceled())
+			client.abort();
+		String path = truncatePath(arg0.getFilePath());
+		monitor.subTask(path);
+		monitor.worked(1);
+	}
+
+	@Override
+	public void fileUpdated(FileUpdatedEvent arg0) {
+		super.fileUpdated(arg0);
+		if (monitor.isCanceled())
+			client.abort();
+		String path = truncatePath(arg0.getFilePath());
+		monitor.subTask(path);
+		monitor.worked(1);
+	}
+
+	private String truncatePath(String path) {
+		String result = path.substring(basePath.length());
+		if (result.length() > 50)
+			return "..." + result.substring(result.length() - 50);
+		return result;
+	}
+	
+	protected String deresolve(String path)
+	{
+		return path.substring(basePath.length());
+	}
+
+}
+
+class DiffListener extends ProgressMonitorListener
 {
 
-    private final IProgressMonitor monitor;
-    private final Client client;
-    private String basePath;
+	private static final long serialVersionUID = 1L;
+	private List<PropertyFileDiff> diffs;
+	
+	
+	public DiffListener(IProgressMonitor monitor, Client client, String basePath) {
+		super(monitor, client, basePath);
+		diffs = new ArrayList<PropertyFileDiff>();
+	}
+	
+	public Collection<PropertyFileDiff> getDiff()
+	{
+		return diffs;
+	}
 
-    public ProgressMonitorListener(IProgressMonitor monitor, Client client, String basePath)
-    {
-        super();
-        this.monitor = monitor;
-        this.client = client;
-        this.basePath = basePath;
-    }
-
-    @Override
-    public void moduleExpanded(ModuleExpansionEvent arg0)
-    {
-        super.moduleExpanded(arg0);
-        monitor.setTaskName(arg0.getModule());
-        monitor.worked(1);
-    }
-
-    @Override
-    public void fileAdded(FileAddedEvent arg0)
-    {
-        super.fileAdded(arg0);
-        if (monitor.isCanceled())
-            client.abort();
-        String path = truncatePath(arg0.getFilePath());
-        monitor.subTask(path);
-        monitor.worked(1);
-    }
-
-    @Override
-    public void fileInfoGenerated(FileInfoEvent arg0)
-    {
-        super.fileInfoGenerated(arg0);
-        if (monitor.isCanceled())
-            client.abort();
-    }
-
-    @Override
-    public void fileRemoved(FileRemovedEvent arg0)
-    {
-        super.fileRemoved(arg0);
-        if (monitor.isCanceled())
-            client.abort();
-        String path = truncatePath(arg0.getFilePath());
-        monitor.subTask(path);
-        monitor.worked(1);
-    }
-
-    @Override
-    public void fileToRemove(FileToRemoveEvent arg0)
-    {
-        super.fileToRemove(arg0);
-        if (monitor.isCanceled())
-            client.abort();
-        String path = truncatePath(arg0.getFilePath());
-        monitor.subTask(path);
-        monitor.worked(1);
-    }
-
-    @Override
-    public void fileUpdated(FileUpdatedEvent arg0)
-    {
-        super.fileUpdated(arg0);
-        if (monitor.isCanceled())
-            client.abort();
-        String path = truncatePath(arg0.getFilePath());
-        monitor.subTask(path);
-        monitor.worked(1);
-    }
-
-
-    private String truncatePath(String path)
-    {
-        String result = path.substring(basePath.length());
-        if (result.length() > 50)
-            return "..." + result.substring(result.length() - 50);
-        return result;
-    }
-
+	@Override
+	public void fileAdded(FileAddedEvent arg0) {
+		PropertyFileDiff diff = PropertiesFactory.eINSTANCE.createPropertyFileDiff();
+		diff.setNewPath(deresolve(arg0.getFilePath()));
+		diff.setKind(DiffKind.ADD);
+		diffs.add(diff);
+	}
+	
+	@Override
+	public void fileRemoved(FileRemovedEvent arg0) {
+		PropertyFileDiff diff = PropertiesFactory.eINSTANCE.createPropertyFileDiff();
+		diff.setOldPath(deresolve(arg0.getFilePath()));
+		diff.setKind(DiffKind.REMOVE);
+		diffs.add(diff);
+	}
+	
+	@Override
+	public void fileUpdated(FileUpdatedEvent arg0) {
+		
+		PropertyFileDiff diff = PropertiesFactory.eINSTANCE.createPropertyFileDiff();
+		diff.setNewPath(deresolve(arg0.getFilePath()));
+		diff.setOldPath(deresolve(arg0.getFilePath()));
+		diff.setKind(DiffKind.MODIFY);
+		diffs.add(diff);
+	}
 }
